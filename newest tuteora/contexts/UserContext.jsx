4eb420290cfe4d2fd5de63ca308601b4ab958890
mintep1-student <mintep1@student.op.ac.nz>
2024-10-22 14:25:30 +1300

    feat: creates login, register, and logout cfunctions in user context

--- newest tuteora/contexts/UserContext.jsx
+++ newest tuteora/contexts/UserContext.jsx
@@ -1,5 +1,11 @@
 import { createContext, useState, useEffect } from "react";
-import { onAuthStateChanged } from "@firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "@firebase/auth";
 import { db, app, auth } from "../services/firebase";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
@@ -7,22 +13,75 @@ export const UserContext = createContext();
 
 export const UserProvider = (props) => {
   const [user, setUser] = useState(null);
+  const [profile, setProfile] = useState(null);
 
-  useEffect(() => {
+  const loadUser = async () => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setUser(user);
     });
-
     return () => unsubscribe();
+  };
+
+  useEffect(() => {
+    loadUser();
   }, [auth]);
 
-  const register = async () => {
-    let newUser;
-    const
-  }
+  const register = async (newUser) => {
+    try {
+      if (
+        !(
+          newUser.accountType &&
+          newUser.email &&
+          newUser.password &&
+          newUser.username &&
+          newUser.firstName &&
+          newUser.lastName
+        )
+      ) {
+        throw new Error("Please fill in all fields");
+      } else if (newUser.password.length < 6) {
+        throw new Error("Password must be at least 6 characters long");
+      } else if (newUser.username.length < 3) {
+        throw new Error("Username must be at least 3 characters long");
+      } else {
+        await createUserWithEmailAndPassword(
+          auth,
+          newUser.email,
+          newUser.password
+        );
+        const userProfile = {
+          id: auth.currentUser.uid,
+          username: newUser.username,
+          email: newUser.email,
+          firstName: newUser.firstName,
+          lastName: newUser.lastName,
+          accountType: newUser.accountType,
+        };
+        setProfile(userProfile);
+      }
+    } catch (error) {
+      console.error("Registration error:", error.message);
+    }
+  };
+
+  const login = async (email, password) => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.error("Login error:", error.message);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await signOut(auth);
+    } catch (error) {
+      console.error("Logout error:", error.message);
+    }
+  };
 
   return (
-    <UserContext.Provider value={{ user, setUser }}>
+    <UserContext.Provider value={{ user, register, profile, login }}>
       {props.children}
     </UserContext.Provider>
   );
