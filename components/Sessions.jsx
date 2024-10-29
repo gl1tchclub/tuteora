4903f77ec2cff28ee62ccc2d56a1d118b9a13ed6
mintep1-student <mintep1@student.op.ac.nz>
2024-10-29 17:48:56 +1300

    chore: moved logout button to dashboard

--- components/Sessions.jsx
+++ components/Sessions.jsx
@@ -1,24 +1,10 @@
 import { View, Text, Button } from "react-native";
-import { useContext } from "react";
-import { UserContext } from "../contexts/UserContext";
 
 const SessionsComponent = ({ navigation }) => {
-  const { logout, user } = useContext(UserContext);
-
-  const handleLogout = async () => {
-    try {
-      await logout();
-      if (user == null) navigation.navigate("Auth");
-    } catch (error) {
-      console.error("Logout error: ", error.message);
-    }
-  };
-
   return (
     <View className="flex-1 justify-center items-center">
       <Text>Sessions Screen</Text>
       <Button onPress={() => navigation.navigate("Home")} title="Go Home" />
-      <Button onPress={handleLogout} title="Log Out" />
     </View>
   );
 };
--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,12 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import { useContext } from "react";
 import { UserContext } from "../contexts/UserContext";
 
 const HomeScreen = () => {
-  const { profile } = useContext(UserContext);
+  const { profile, logout, user } = useContext(UserContext);
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      if (user == null) navigation.navigate("Auth");
+    } catch (error) {
+      console.error("Logout error: ", error.message);
+    }
+  };
 
   return (
-    <View className="flex-1 items-center h-full bg-neutral-100 ">
+    <View className="flex-1 items-center h-full bg-neutral-100">
       <Text className="text-3xl py-6">Welcome, {profile.firstName}</Text>
       <View className="bg-white p-4 w-2/3 mt-10 h-max rounded-xl">
         <Text className="text-lg bg-neutral-200 rounded-lg p-2 my-4">
@@ -18,6 +27,10 @@ const HomeScreen = () => {
         <Text className="text-lg bg-neutral-200 rounded-lg p-2 my-4">
           Account: {profile.accountType}
         </Text>
+        <Button
+          onPress={handleLogout}
+          title="Log Out"
+        />
       </View>
     </View>
   );
