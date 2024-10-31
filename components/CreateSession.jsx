f2a1541505e59846794bf0e371df5fa803668dec
mintep1-student <mintep1@student.op.ac.nz>
2024-10-31 17:07:49 +1300

    chore: Update CreateSession component with react-hook-form integration

--- components/CreateSession.jsx
+++ components/CreateSession.jsx
@@ -1,11 +1,21 @@
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { View, Text, Button } from "react-native";
+import { useForm } from "react-hook-form";
+import { useState, useEffect } from "react";
 
 const CreateSession = ({ navigation }) => {
-
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => console.log(data);
   return (
-    <View className="flex-1 justify-center items-center">
-      <MaterialCommunityIcons name="file-document-edit" size={40} color="black" />
+    <View className="flex-1 w-full p-4">
+      <MaterialCommunityIcons
+        name="file-document-edit"
+        size={40}
+        color="black"
+      />
+      <form>
+        <input  />
+      </form>
       <Text>Create Screen</Text>
     </View>
   );
