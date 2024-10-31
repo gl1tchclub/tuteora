f205a71555b65fa0c9d251d9850cae2d8d2144d0
mintep1-student <mintep1@student.op.ac.nz>
2024-10-31 16:23:57 +1300

    refactor: Update SessionsComponent UI and navigation

--- components/Sessions.jsx
+++ components/Sessions.jsx
@@ -1,16 +1,19 @@
 import { View, Text, Button } from "react-native";
 
 const SessionsComponent = ({ navigation }) => {
-
   const handleCreateSession = () => {
     navigation.navigate("CreateSession");
-  }
+  };
 
   return (
     <View className="flex-1 justify-center items-center">
-      <Text>Sessions Screen</Text>
-      <View className="mt-4 rounded-xl bg-white">
-        <Button title="Create Session" onPress={handleCreateSession}/>
+      <View className="bg-white p-4 w-2/3 mt-10 h-max rounded-xl">
+        <Text className="text-lg p-2 my-2">Next Session:</Text>
+        <Button
+          title="Create Session"
+          onPress={handleCreateSession}
+          color="#46ab61"
+        />
       </View>
     </View>
   );
