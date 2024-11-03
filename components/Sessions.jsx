09862a95f4e165025f93023a32958e1e594b6922
mintep1-student <mintep1@student.op.ac.nz>
2024-11-04 12:43:21 +1300

    refactor: Update SessionsComponent to use NextSessionHeader component for displaying the next session details, change from flatlist to scroll view

--- components/Sessions.jsx
+++ components/Sessions.jsx
@@ -1,7 +1,15 @@
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { View, ScrollView, Text, Button, FlatList, TouchableOpacity } from "react-native";
+import {
+  View,
+  ScrollView,
+  Text,
+  Button,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 import { useState, useEffect, useContext } from "react";
 import { SessionContext } from "../contexts/SessionsContext";
+import { UserContext } from "../contexts/UserContext";
 
 const SessionsComponent = ({ navigation }) => {
   const [sessions, setSessions] = useState([
@@ -14,7 +22,7 @@ const SessionsComponent = ({ navigation }) => {
       location: "Online",
     },
     {
-      tutor: "Terry Smith",
+      tutor: "John Doe",
       student: "Joseph Dunn",
       topic: "IT",
       date: "11/13/2024",
@@ -22,6 +30,8 @@ const SessionsComponent = ({ navigation }) => {
       location: "Online",
     },
   ]);
+
+  const { profile } = useContext(UserContext);
 
   //const that holds the session with the earliest date/time
   // Function to parse date and time into a Date object
@@ -44,24 +54,27 @@ const SessionsComponent = ({ navigation }) => {
     setSessions(sessions.filter((_, i) => i !== index));
   };
 
+  const NextSessionHeader = () => (
+    <View className="bg-white p-4 my-2 rounded-xl w-4/5 self-center">
+      <Text className="text-lg mb-2">Next Session:</Text>
+      <Text className="text-md mb-2">
+        <Text className="font-bold">Tutor:</Text> {earliestSession.tutor}
+        {"\n"}
+        <Text className="font-bold">Student:</Text> {earliestSession.student}
+        {"\n"}
+        <Text className="font-bold">Date:</Text> {earliestSession.date}
+        {"\n"}
+        <Text className="font-bold">Time:</Text> {earliestSession.time}
+        {"\n"}
+        <Text className="font-bold">Location:</Text> {earliestSession.location}
+      </Text>
+    </View>
+  );
+
   return (
-    <ScrollView className="flex-1 justify-center items-center p-4">
-      <View className="bg-white p-4 my-2 rounded-xl w-4/5">
-        <Text className="text-lg mb-2 font-bold">Next Session:</Text>
-        <Text className="text-md mb-2">
-          <Text className="font-bold">Tutor:</Text> {earliestSession.tutor}
-          {"\n"}
-          <Text className="font-bold">Student:</Text> {earliestSession.student}
-          {"\n"}
-          <Text className="font-bold">Date:</Text> {earliestSession.date}
-          {"\n"}
-          <Text className="font-bold">Time:</Text> {earliestSession.time}
-          {"\n"}
-          <Text className="font-bold">Location:</Text>{" "}
-          {earliestSession.location}
-        </Text>
-      </View>
-      <View className="bg-white p-4 my-2 rounded-xl w-4/5">
+    <ScrollView contentContainerStyle="pt-5">
+      <NextSessionHeader />
+      <View className="bg-white p-4 my-2 rounded-xl w-4/5 self-center">
         <View className="flex-row justify-between items-center mb-4">
           <Text className="text-lg font-bold">Schedule</Text>
           <Button
@@ -70,32 +83,27 @@ const SessionsComponent = ({ navigation }) => {
             color="#46ab61"
           />
         </View>
-        <FlatList
-          data={sessions}
-          keyExtractor={(item, index) => index.toString()}
-          renderItem={({ item, index }) => (
-            <View className="bg-green-100 p-4 my-2 rounded-xl">
-              <Text className="text-md mb-2">
-                <Text className="font-bold">Tutor:</Text> {item.tutor}
-                {"\n"}
-                <Text className="font-bold">Student:</Text> {item.student}
-                {"\n"}
-                <Text className="font-bold">Date:</Text> {item.date}
-                {"\n"}
-                <Text className="font-bold">Time:</Text> {item.time}
-                {"\n"}
-                <Text className="font-bold">Location:</Text> {item.location}
-              </Text>
-              <TouchableOpacity
-                onPress={() => handleDeleteSession(index)}
-                className="bg-red-500 p-2 rounded"
-              >
-                <Text className="text-white text-center">Delete</Text>
-              </TouchableOpacity>
-            </View>
-          )}
-          contentContainerStyle="pt-5"
-        />
+        {sessions.map((item, index) => (
+          <View key={index} className="bg-green-100 p-4 my-2 rounded-xl">
+            <Text className="text-md mb-2">
+              <Text className="font-bold">Tutor:</Text> {item.tutor}
+              {"\n"}
+              <Text className="font-bold">Student:</Text> {item.student}
+              {"\n"}
+              <Text className="font-bold">Date:</Text> {item.date}
+              {"\n"}
+              <Text className="font-bold">Time:</Text> {item.time}
+              {"\n"}
+              <Text className="font-bold">Location:</Text> {item.location}
+            </Text>
+            <TouchableOpacity
+              onPress={() => handleDeleteSession(index)}
+              className="bg-red-500 p-2 rounded"
+            >
+              <Text className="text-white text-center">Delete</Text>
+            </TouchableOpacity>
+          </View>
+        ))}
       </View>
     </ScrollView>
   );
