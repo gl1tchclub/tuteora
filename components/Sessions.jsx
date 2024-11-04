d61e8561482c0e906cdf15d32dfbb55070dfeefb
mintep1-student <mintep1@student.op.ac.nz>
2024-11-04 13:29:08 +1300

    style: minor style changes

--- components/Sessions.jsx
+++ components/Sessions.jsx
@@ -60,7 +60,7 @@ const SessionsComponent = ({ navigation }) => {
   const SessionWidget = (props) => {
     return (
       <View className="bg-green-100 p-3 my-2 rounded-xl">
-        <View className="flex-row mb-2">
+        <View className="flex-row justify-between mb-4">
           {profile.accountType === "Tutor" ? (
             <>
               <Text className="font-bold mx-2">Student:</Text>
@@ -75,7 +75,7 @@ const SessionsComponent = ({ navigation }) => {
           <Text className="font-bold ml-4 mr-2">Location:</Text>
           <Text>{props.location}</Text>
         </View>
-        <View className="flex-row space-x-4">
+        <View className="flex-row justify-between">
           <Text className="font-bold ml-2">Date:</Text>
           <Text>{props.date}</Text>
           <Text className="font-bold pl-9">Time:</Text>
