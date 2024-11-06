96f1cf789913e92cde71e76c11ef1e0d7e83844d
mintep1-student <mintep1@student.op.ac.nz>
2024-11-06 13:58:42 +1300

    refactor: Update TutorDetails component to include random background color for tutor details section

--- components/TutorDetails.jsx
+++ components/TutorDetails.jsx
@@ -7,11 +7,11 @@ const TutorDetails = (props) => {
   const { profile, updateProfile } = useContext(UserContext);
   const [request, setRequest] = useState(null);
   const [topic, setTopic] = useState(null);
-  const tutor = props.tutor;
-  
+  const tutor = props.tutor.tutor;
+
   const getRandomColor = () => {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
+    const letters = "0123456789ABCDEF";
+    let color = "#";
     for (let i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
     }
@@ -24,27 +24,68 @@ const TutorDetails = (props) => {
 
   return (
     <ScrollView className="flex-1 bg-white w-full">
-      <View className="w-full h-2/3 mb-3 bg-cover bg-center bg-no-repeat" style={{ backgroundColor: randomColor, opacity: 0.6 }} />
-      <Text className="text-2xl font-bold mb-2">
-        {tutor.firstName} {tutor.lastName}{" "}
-        <TouchableOpacity
-          onPress={() => handleRequestTutor(tutor.id)}
-          className="bg-green-500 rounded w-fit-content self-center mr-2"
-        >
-          <MaterialCommunityIcons name="check" size={24} color="white" />
-        </TouchableOpacity>
-      </Text>
-      <Text className="text-lg mb-3">Bio</Text>
-      <Text className="text-base mb-8">{tutor.bio || "No Bio"}</Text>
-      <Text className="text-lg mb-3">Studying: {tutor.study}</Text>
-      <Text className="text-xl font-bold mb-2">Topics:</Text>
-      {tutor.topics && tutor.topics.map((topic, idx) => (
-        <Text key={idx} className="text-base mb-1">
-          - {topic}
+      <View className="relative w-full h-48 mb-3">
+        <View
+          className="absolute inset-0 bg-cover bg-center bg-no-repeat w-full h-full"
+          style={{ backgroundColor: randomColor, opacity: 0.6 }}
+        />
+        <View className="absolute bottom-0 left-0 flex-row justify-between w-full">
+          <View
+            className="rounded-lg mb-4 ml-4 border border-neutral-400/[0.1]"
+            style={{
+              backgroundColor: randomColor,
+              shadowColor: "#00000",
+              shadowOffset: { width: 2, height: 2 },
+              shadowOpacity: 0.9,
+              shadowRadius: 2,
+              elevation: 3,
+            }}
+          >
+            <Text className="text-2xl font-bold text-white p-3">
+              {tutor.firstName} {tutor.lastName}
+            </Text>
+          </View>
+          <TouchableOpacity
+            onPress={() => handleRequestTutor(tutor.id)}
+            className="bg-green-500 rounded-lg w-fit-content self-center mx-4 flex-row items-center border border-neutral-400/[0.1]"
+            style={{
+              shadowColor: "#000000",
+              shadowOffset: { width: 2, height: 2 },
+              shadowOpacity: 0.9,
+              shadowRadius: 2,
+              elevation: 3,
+            }}
+          >
+            <MaterialCommunityIcons
+              name="account-arrow-right"
+              size={24}
+              color="white"
+              style={{ paddingHorizontal: 4, paddingVertical: 2 }}
+            />
+            <Text className="text-white px-2 text-lg">Request Tutoring</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View className="w-full flex-1 justify-center">
+        <View className="w-full p-4 bg-green-200">
+          <Text className="text-xl mb-3 font-bold">About Me</Text>
+          <Text className="text-base mb-8">{tutor.bio || "No Bio"}</Text>
+        </View>
+        <View className="w-full flex-grow">
+          <Text className="text-lg mb-3">Studying: {tutor.study}</Text>
+          <Text className="text-xl font-bold mb-2">Topics:</Text>
+          {tutor.topics &&
+            tutor.topics.map((topic, idx) => (
+              <Text key={idx} className="text-base mb-1">
+                - {topic}
+              </Text>
+            ))}
+        </View>
+        <Text className="text-xl font-bold mb-2">Availability:</Text>
+        <Text className="text-base mb-8">
+          {tutor.availability || "No Schedule Set"}
         </Text>
-      ))}
-      <Text className="text-xl font-bold mb-2">Availability:</Text>
-      <Text className="text-base mb-8">{tutor.availability || "No Schedule Set"}</Text>
+      </View>
     </ScrollView>
   );
 };
