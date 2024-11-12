5cede8ac4451664f7c9cf57542cb17454d0f143e
mintep1-student <mintep1@student.op.ac.nz>
2024-11-12 15:59:35 +1300

    feat: Add topics field to UpdateTutor form, update UI

--- components/UpdateTutor.jsx
+++ components/UpdateTutor.jsx
@@ -13,6 +13,7 @@ const UpdateTutor = ({ navigation }) => {
   const [bio, setBio] = useState(profile.bio || "");
   const [isAvailable, setIsAvailable] = useState(profile.isAvailable);
   const [availability, setAvailability] = useState(profile.availability || []);
+  const [topics, setTopics] = useState(profile.topics || []);
   const days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
 
   const handleUpdate = async (tutor) => {
@@ -39,25 +40,36 @@ const UpdateTutor = ({ navigation }) => {
         className="flex-1 w-11/12 m-12 rounded-lg bg-white p-6 self-center"
         style={{ elevation: 5 }}
       >
+        <Text className="text-2xl font-bold self-center">Tutor Details</Text>
+        <Text className="text-neutral-500 self-center mb-2">
+          This is what potential students will see
+        </Text>
         <View className="w-full flex-1 pb-2">
           <TextInput
             className="h-10 rounded-lg border-2 border-[#46ab61] px-4 text-lg mt-4"
             value={bio}
             onChangeText={setBio}
-            placeholder={profile.bio || "Bio"}
+            placeholder={profile.bio || "Bio (200 characters)"}
+            maxLength={200}
           />
-          <Text className="mt-4 self-center text-lg">Availability</Text>
+          <View className="border-b-neutral-300 border-b mt-6 w-full self-center" />
+          <Text className="mt-6 self-center text-lg">Availability</Text>
           <View className="flex-row space-x-4 flex-wrap mt-4 w-full">
             {days.map((day, index) => (
               <View key={index} className="flex-row space-x-2">
                 <Text>{day}</Text>
                 <Checkbox
-                  value={availability}
-                  onValueChange={(option) => handleAvailability(option)}
+                  value={availability.includes(day)}
+                  onValueChange={() => handleAvailability(day)}
                 />
               </View>
             ))}
           </View>
+          <View className="border-b-neutral-300 border-b mt-6 w-full self-center" />
+          <View>
+            <Text className="mt-6 self-center text-lg">Topics</Text>
+          </View>
+          <View className="border-b-neutral-300 border-b mt-6 w-full self-center" />
         </View>
       </View>
     </ScrollView>
